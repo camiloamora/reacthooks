9955a2737bb5c64fb761c0e42b262925fe9ff13b
Camilo Mora <kalufau@hotmail.com>
2021-01-14 22:39:16 -0500

    Add base line, component Header with useState

---
+++ src/components/Header.jsx
@@ -0,0 +1,20 @@
+import React, { useState } from 'react';
+
+const Header = () => {
+
+    const [darkMode, setDarkMode] = useState(false);
+
+    const handleClick = () => {
+        setDarkMode(!darkMode);
+    }
+
+    return (
+      <div className="Header">
+          <h1>ReactHooks</h1>
+          <button type="button" onClick={handleClick}>{darkMode ? 'Dark Mode' : 'Ligth Mode'}</button>
+          <button type="button" onClick={() => setDarkMode(!darkMode)}>{darkMode ? 'Dark Mode2' : 'Ligth Mode2'}</button>
+      </div>
+    );
+}
+
+export default Header;
